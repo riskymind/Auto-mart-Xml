8f8e24bc0337811728283564f80abb2107c82424
kelechi <oparaiskelechi@gmail.com>
2023-03-11 22:30:40 +0100

    design most screens

--- app/src/main/java/com/asterisk/automart/MainActivity.kt
+++ app/src/main/java/com/asterisk/automart/MainActivity.kt
@@ -2,10 +2,20 @@ package com.asterisk.automart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.asterisk.automart.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var biniding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        biniding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(biniding.root)
+
+        biniding.apply {
+            bottomNavigationView.apply {
+                background = null
+                menu.getItem(1).isEnabled = false
+            }
+        }
     }
 }
--- app/src/main/java/com/asterisk/automart/home/HomeActivity.kt
+++ app/src/main/java/com/asterisk/automart/home/HomeActivity.kt
@@ -1,12 +1,12 @@
-package com.asterisk.automart.seller
+package com.asterisk.automart.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.asterisk.automart.R
 
-class SellerActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_seller)
+        setContentView(R.layout.activity_home)
     }
 }
